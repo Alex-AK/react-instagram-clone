624f49c60b20ff53ef2a6555bc1d9698e24dd02b
Kingaa6 <Kingaa6@gmail.com>
2019-01-14 12:42:55 -0800

    Data imported and confirmed working

--- instagram/src/App.jsx
+++ instagram/src/App.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import dummyData from './dummy-data';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: dummyData
+    };
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Learn React</h1>
+          <h1>{this.state.data[0].username}</h1>
         </header>
       </div>
     );
