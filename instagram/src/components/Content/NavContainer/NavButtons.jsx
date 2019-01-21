68036bf3893c2bf739d381f9e1340a73559d9447
Kingaa6 <Kingaa6@gmail.com>
2019-01-21 12:14:20 -0800

    Logout button added

--- instagram/src/components/Content/NavContainer/NavButtons.jsx
+++ instagram/src/components/Content/NavContainer/NavButtons.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from 'styled-components';
 
 import navButtons from '../../../assets/ig-nav-buttons.png';
+import logoutButton from '../../../assets/logout.png';
 
 const NavButtonDiv = styles.div`
   margin-right: 40px;
@@ -11,14 +12,41 @@ const NavButtonImg = styles.img`
   height: 25px;
   margin: 0 5px;
 `;
+const LogoutButtonImg = styles.img`
+  height: 24px;
+  margin: 3px 0 0 15px;
 
-const NavButtons = props => {
-  return (
-    <NavButtonDiv>
-      <NavButtonImg src={navButtons} alt="navigation buttons" />
-    </NavButtonDiv>
-  );
-};
+  &:hover {
+    cursor: pointer
+  };
+`;
+
+class NavButtons extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoggedIn: true
+    };
+  }
+
+  handleLogout = () => {
+    localStorage.clear();
+    window.location.reload();
+  };
+
+  render() {
+    return (
+      <NavButtonDiv>
+        <NavButtonImg src={navButtons} alt="navigation buttons" />
+        <LogoutButtonImg
+          src={logoutButton}
+          alt="navigation buttons"
+          onClick={this.handleLogout}
+        />
+      </NavButtonDiv>
+    );
+  }
+}
 
 // nav button component does not receive props
 
